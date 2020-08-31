1266d5a69621f965aa076a910369c9136b651924
Henrique1818 <luiz.henrique01827@gmail.com>
2020-08-30 23:11:37 -0300

    page create Book

--- src/pages/CreateBook/index.js
+++ src/pages/CreateBook/index.js
@@ -1,8 +1,97 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import api from '../../services/api';
+
+import Header from '../../components/Header';
+import Develop from '../../assets/develop.svg';
+
+import * as S from './styled';
+import { Link } from 'react-router-dom';
 
 function CreateBook() {
+    const [avatarUrl, setAvatarUrl] = useState()
+    const [title, setTitle] = useState()
+    const [description, setDescription] = useState()
+    const [publishingCompany, setPublishingCompany] = useState()
+    const [authors, setAuthors] = useState()
+    const [category, setCategory] = useState()
+
+    async function Save() {
+        await api.post('/library', {
+            avatar_url: avatarUrl,
+            title,
+            description,
+            publishing_company: publishingCompany,
+            authors,
+            category
+        }).then(() => {
+            alert('Livro cadastrado com sucesso')
+        })
+    }
+
     return (
-        <h3>Página de Formulários</h3>
+        <>
+            <S.Container>
+                <Header />
+                <S.Input>
+                    <div className="image-container">
+                        <img src={Develop} alt="Image de ilustração de login"/>
+                    </div>
+
+                    <div className="form-container">
+                        <div className="content">
+                            <h2>Avatar</h2>
+                            <input type="text" placeholder="http://" 
+                                onChange={e => setAvatarUrl(e.target.value)}
+                                value={avatarUrl}
+                            />
+                        </div>
+
+                        <div className="content">
+                            <h2>Título</h2>
+                            <input type="text" placeholder="Nome do Livro" 
+                                onChange={e => setTitle(e.target.value)}
+                                value={title}
+                            />
+                        </div>
+
+                        <div className="content">
+                            <h2>Descrição</h2>
+                            <textarea rows="5" placeholder="Resumo sobre o Livro" 
+                                onChange={e => setDescription(e.target.value)}
+                                value={description}
+                            />
+                        </div>
+
+                        <div className="content">
+                            <h2>Editora</h2>
+                            <input type="text" placeholder="Nome da editora" 
+                                onChange={e => setPublishingCompany(e.target.value)}
+                                value={publishingCompany}
+                            />
+                        </div>
+
+
+                        <div className="content">
+                            <h2>Autor(a)</h2>
+                            <input type="text" placeholder="Nome do autor(a)" 
+                                onChange={e => setAuthors(e.target.value)}
+                                value={authors}
+                            />
+                        </div>
+
+                        <div className="content">
+                            <h2>Categoria</h2>
+                            <input type="text" placeholder="Nome da categoria ex: fantasia" 
+                                onChange={e => setCategory(e.target.value)}
+                                value={category}
+                            />
+                        </div>
+                        <Link to="/listBook" className="btn-save" onClick={Save}>Salvar</Link>
+                    </div>
+                </S.Input>
+
+            </S.Container>
+        </>
     )
 }
 
