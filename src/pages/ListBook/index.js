3c0ac4fb1390add4c89b289ad2b5cc237c66cc6d
Henrique1818 <luiz.henrique01827@gmail.com>
2020-08-30 23:14:02 -0300

    page List Book

--- src/pages/ListBook/index.js
+++ src/pages/ListBook/index.js
@@ -24,25 +24,25 @@ function ListBook() {
     }, [])
     
     return (
-        <S.Container>
+        <>
             <Header />
             <Filter />
+            <S.Container>
+                <S.ShowBook>
+                    <div className="main">
+                        <h2>Livros</h2>
 
-            <S.ShowBook>
-               <div className="main">
-                    <h2>Livros</h2>
-
-                    <div className="content">
-                        {
-                            books.map(book => (
-                                <CardBook avatar_url={book.avatar_url} title={book.title} id={book._id} />
-                            ))
-                        }
+                        <div className="content">
+                            {
+                                books.map(book => (
+                                    <CardBook key={book._id} avatar_url={book.avatar_url} title={book.title} id={book._id} />
+                                ))
+                            }
+                        </div>
                     </div>
-               </div>
-            </S.ShowBook>
-            
-        </S.Container>
+                </S.ShowBook>
+            </S.Container>
+        </>
     );
 };
 
