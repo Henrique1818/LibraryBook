7dbede767429b265ec9db62e28985125f3f17c06
Henrique1818 <luiz.henrique01827@gmail.com>
2020-09-01 10:13:46 -0300

    components Book

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -3,18 +3,18 @@ import React from 'react';
 import * as S from './styled';
 import { Link } from 'react-router-dom';
 
-function Book({ avatar_url, title, description, authors, publishing_company, category}) {
+function Book({ avatar_url, title, description, authors, publishing_company, category, id}) {
     return (
         <S.Container>
             <div className="image-container">
                 <img src={avatar_url} alt={title}/>
-                <Link to="/bookCreate" className="btn-editar">Editar</Link>
+                <Link to={`/library/books/update/${id} `} className="btn-editar">Editar</Link>
             </div>
 
             <div className="content">
                 <h2>{title}</h2>
                 <p className="description">{description}</p>
-                <h3>Author: {authors} <span>Editora: {publishing_company}</span></h3>
+                <h3>Autor(a): {authors} <span>Editora: {publishing_company}</span></h3>
                 <p className="category"> Categoria: <span>{category}</span></p>
             </div>
         </S.Container>
